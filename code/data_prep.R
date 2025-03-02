694f885463fa31ac957ee0dbc89d5d20857d93e0
juliatdm7 <juliatdm7@gmail.com>
2025-03-02 20:52:05 +0100

    Selecting variables to include in data subsets

--- code/data_prep.R
+++ code/data_prep.R
@@ -14,7 +14,11 @@ birdphen <- read.csv("data/Bird_Phenology.csv")  # loading phenology data
 birdphenx <- as_tibble(birdphen)  #converting data into a tibble (easier to manage)
 birdphenx  # visualising first rows of data
 
+# Selecting variables that will be useful for the project from the phenology data
+blutiphen <- birdphenx %>% filter(species == "bluti")  # first, we will select and extract cases that are of blue tits
+# Since we aim to study breeding success trends in blue tit populations across age groups and across sites, we will extract those variables that may be useful to study breeding success: first egg lay date ("fed"), number of hatched eggs ("number.hatched") and fledling success (suc)
+blutiphen <- blutiphen %>% select(year, site, box, fed, number.hatched, suc)  # we also select columns that will help us collide both databases into one by identifying individuals (i.e.: year, site and box)
 
-
-
+# Selecting variables that will be useful for the project from the adult data
+blutiadults <- adultx %>% select(ring, year, site, box, age, sex)
 

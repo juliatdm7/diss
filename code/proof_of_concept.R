8f4031608cef90e55496fe0473bd4e3522e45a16
juliatdm7 <juliatdm7@gmail.com>
2025-03-12 16:44:10 +0100

    Adding new figures.

--- code/proof_of_concept.R
+++ code/proof_of_concept.R
@@ -7,3 +7,6 @@ y <- c(805, 197, 78, 36, 17, 0, 0, 1)  # I need to change this so that it direct
 brattempts_age <- data.frame(x, y)
 
 plot(brattempts_age, type = "b", xlab="Number of breeding attempts recorded", ylab="Number of females")
+
+bluti2$suc <- as.numeric(bluti2$suc)
+hist(bluti2$suc, xlab = "Number of chicks successfully fledged", col = "#C0AFE2", xlim=c(0,14),ylim = c(0,250), main = NULL)

8dae7f344297cf554292a8b435dc67c0b1a5dd89
juliatdm7 <juliatdm7@gmail.com>
2025-03-29 00:17:08 +0100

    Creating graphs to present the data in different ways.

--- code/proof_of_concept.R
+++ code/proof_of_concept.R
@@ -2,21 +2,173 @@
 ### PROOF OF CONCEPT ###
 ########################
 
+library(ggplot2)
+
+
 x <- c(1:7)  # Number of breeding attempts
 y <- c(811, 192, 78, 36, 16, 0, 1)  # I need to change this so that it directly references the database
 brattempts_age <- data.frame(x, y)
 
+# Number of females for which we have re
 plot(brattempts_age, type = "b", xlab="Number of breeding attempts recorded", ylab="Number of females")
 
 bluti2$suc <- as.numeric(bluti2$suc)
+
+### Distribution of the breeding trait variables ###
+
+# Fledgeling success
 ggplot(bluti2, aes(suc)) +
   geom_histogram(colour = "black", fill = "#248fc9", binwidth=1) +
   labs(x = "Nr of chicks fledged successfully", y = "Frequency") +
   theme_bw() +
   theme(axis.title = element_text(size = 14), axis.text = element_text(size = 12))
 
+# Lay date of first egg
+ggplot(bluti2, aes(fed)) +
+  geom_histogram(colour = "black", fill = "#248fc9", binwidth=1) +
+  labs(x = "Lay date of first egg (1 = Jan 1st)", y = "Frequency") +
+  theme_bw() +
+  theme(axis.title = element_text(size = 14), axis.text = element_text(size = 12))
 
-### Map of study sites ###
+# Clutch size
+ggplot(bluti2, aes(cs)) +
+  geom_histogram(colour = "black", fill = "#248fc9", binwidth=1) +
+  labs(x = "Clutch size", y = "Frequency") +
+  theme_bw() +
+  theme(axis.title = element_text(size = 14), axis.text = element_text(size = 12))
+
+### Age-specific patterns of breeding traits ###
+
+# Violin plot for fledgeling success
+ggplot(bluti2, aes(x=y_old, y=suc, group=y_old)) +
+  geom_violin(aes(fill=factor(y_old), colour = "black"),stat = "ydensity", position = "dodge", colour = "black", scale = "count", adjust=0.7) +
+  labs(x = "Age of female parent", y = "Number of chicks successfully fledged") +
+  theme_bw() +
+  guides(fill=guide_legend(title="Age in years old")) +
+  scale_x_continuous(breaks=seq(1,7,1))+
+  scale_y_continuous(breaks=seq(1,14,1))
+
+# Violin plot for lay date of first egg
+ggplot(bluti2, aes(x=y_old, y=fed, group=y_old)) +
+  geom_violin(aes(fill=factor(y_old), colour = "black"),stat = "ydensity", position = "dodge", colour = "black", scale = "count", adjust=0.7) +
+  labs(x = "Age of female parent", y = "First egg lay date (ordinal numbers)") +
+  theme_bw() +
+  guides(fill=guide_legend(title="Age in years old"))
+
+# Violin plot for clutch size
+ggplot(bluti2, aes(x=y_old, y=cs, group=y_old)) +
+  geom_violin(aes(fill=factor(y_old), colour = "black"),stat = "ydensity", position = "dodge", colour = "black", scale = "count", adjust=0.7) +
+  labs(x = "Age of female parent", y = "Clutch size") +
+  theme_bw() +
+  guides(fill=guide_legend(title="Age in years old")) +
+  scale_x_continuous(breaks=seq(1,7,1)) +
+  scale_y_continuous(breaks=seq(0,14,1))
+
+# Now, I'll create new dataframes that will include the mean values for each breeding trait for each age group (years old) and their corresponding standard deviation
+
+## For fledgeling success
+bluti2_suc_summary <- bluti2 %>%
+  group_by(y_old) %>%
+  summarise_at(vars(suc), list(mean=mean, sd=sd)) %>% 
+  as.data.frame()
+## For lay date of first egg
+bluti2_fed_summary <- bluti2 %>%
+  group_by(y_old) %>%
+  na.omit(bluti2_fed_summary) %>%
+  summarise_at(vars(fed), list(mean=mean, sd=sd)) %>% 
+  as.data.frame()
+## For clutch size
+bluti2_cs_summary <- bluti2 %>%
+  group_by(y_old) %>%
+  na.omit(bluti2_fed_summary) %>%
+  summarise_at(vars(cs), list(mean=mean, sd=sd)) %>% 
+  as.data.frame()
+
+# Scatter plot of fledgeling success per age group
+ggplot(bluti2, aes(x=y_old, y=fed, fill=factor(w))) +
+  geom_point(aes(fill=w, alpha = 0.01)) +
+  labs(x = "Age of female parent", y = "First egg lay date (ordinal numbers)") +
+  theme_bw() +
+  scale_x_continuous(breaks=seq(1,7,1)) 
+
+# Barplot with SD bars for fledgeling success
+ggplot(bluti2_suc_summary, aes(x=y_old, y=mean, fill = factor(y_old))) +
+  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd)) +
+  geom_col(colour = "black") +
+  labs(x = "Age of female parent", y = "Number of chicks successfully fledged") +
+  theme_bw() +
+  scale_x_continuous(breaks=seq(1,7,1))+
+  scale_y_continuous(breaks=seq(0,14,1), expand = expansion(mult = c(0, .1)))
+
+# Barplot with SD bars for fledgeling success
+ggplot(bluti2_fed_summary, aes(x=y_old, y=mean, fill = factor(y_old))) +
+  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd)) +
+  geom_col(colour = "black") +
+  labs(x = "Age of female parent", y = "Number of chicks successfully fledged") +
+  theme_bw() +
+  scale_x_continuous(breaks=seq(1,7,1)) +
+  scale_y_continuous(breaks=seq(0,200,10), expand = expansion(mult = c(0, .1)))
+
+# Barplot with SD bars for clutch size
+ggplot(bluti2_cs_summary, aes(x=y_old, y=mean, fill = factor(y_old))) +
+  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd)) +
+  geom_col(colour = "black") +
+  labs(x = "Age of female parent", y = "Clutch size") +
+  theme_bw() +
+  scale_x_continuous(breaks=seq(1,7,1)) +
+  scale_y_continuous(breaks=seq(0,12,1),expand = expansion(mult = c(0, .1)))
+
+
+# Geom_smooth() for fledgeling success
+ggplot(bluti2_suc_summary, aes(x=y_old, y=mean)) +
+  geom_point(colour = "black", size = 3) +
+  geom_smooth(linewidth=0.8, linetype = 2, col="#248fc9") +
+  labs(x = "Age of female parent", y = "Number of chicks successfully fledged") +
+  theme_bw() +
+  scale_x_continuous(breaks=seq(1,7,1))+
+  scale_y_continuous(breaks=seq(0,14,1))
+
+# Geom_smooth() for lay date of first egg
+ggplot(bluti2_fed_summary, aes(x=y_old, y=mean)) +
+  geom_point(colour = "black", size = 3) +
+  geom_smooth(linewidth=0.8, linetype = 2, col="#248fc9") +
+  labs(x = "Age of female parent", y = "Lay date of first egg (1 = Jan 1st)") +
+  theme_bw() +
+  scale_x_continuous(breaks=seq(1,7,1))+
+  scale_y_continuous(breaks=seq(0,14,1))  # for some reason, in this graph my y-axis values are gone
+
+# Geom_smooth() for clutch size
+ggplot(bluti2_cs_summary, aes(x=y_old, y=mean)) +
+  geom_point(colour = "black", size = 3) +
+  geom_smooth(linewidth=0.8, linetype = 2, col="#248fc9") +
+  labs(x = "Age of female parent", y = "Clutch size") +
+  theme_bw() +
+  scale_x_continuous(breaks=seq(1,7,1))+
+  scale_y_continuous(breaks=seq(0,14,1))
+
+
+### Distinct birds across years and sites ###
+
+# Distinct rings:
+bluti2_distinctring <- bluti2 %>% 
+  distinct(ring,.keep_all=T)
+
+# Distinct birds across sites
+ggplot(bluti2_distinctring,aes(x=site, fill=site)) +
+  geom_bar() +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  scale_y_continuous(breaks=seq(0,60,5), expand=expansion(mult=c(0,0.1)))
+
+# Distinct birds across years
+ggplot(bluti2_distinctring,aes(x=year, fill=factor(year))) +
+  geom_bar() +
+  theme(axis.text.x = element_text(angle = 60, vjust = 0.5, hjust=1)) +
+  theme_bw() +
+  scale_x_continuous(breaks=seq(2014,2024,1)) +
+  scale_y_continuous(breaks=seq(0,180,25), expand=expansion(mult=c(0,0.1)))
+
+### Map of study sites ###### Map of study_oldy sites ###
 
 install.packages("terra",dependencies=TRUE,repos="https://cloud.r-project.org")
 library(terra)

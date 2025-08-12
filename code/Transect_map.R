ae5598d319b99959c58809c5366e2955303ed298
juliatdm7 <juliatdm7@gmail.com>
2025-08-12 20:19:58 +0200

    Small changes to map code

--- code/Transect_map.R
+++ code/Transect_map.R
@@ -19,12 +19,15 @@ sites <- read.csv("data/site_detailsII.csv") %>%
 
 sites$lat <- c(55.977, 56.02237, 56.05684, 56.12171, 56.1746, 56.16253, 56.30158, 56.35109, 56.35749, 56.42266, 56.47694, 56.53707, 56.57103, 56.64949, 56.681, 56.73079, 56.73073, 56.76043, 56.77057, 56.79876, 56.81636, 56.83151, 56.9207, 56.99202, 57.05359, 57.10939, 57.07229, 57.11714, 57.14681, 57.18645, 57.21044, 57.28932, 57.2966, 57.32509, 57.41388, 57.5084, 57.5528, 57.6398, 57.69372, 57.72381, 57.79614, 57.87125, 57.8776, 57.8853)
 
+
 ggplot() +
   geom_sf(data = scotland, fill = "lightgray", alpha = 0.5, linewidth = 0.35, colour="black") + # borders of Scotland
-  geom_sf(data = sites, pch = 16, size = 2, color = "#248fc9", alpha = 0.7) + #Transect sites
+  geom_sf(data = sites, pch = 16, size = 2, aes(color = as.factor(Current.Boxes)), alpha = 1) + #Transect sites
   theme_bw() +  # Gridlines and latitude as axes
   labs(x="Longitude", y="Latitude") +
-  coord_sf(xlim = c(-8, -1), ylim = c(54.6, 59.55)) 
-ggsave("figures/sitesMap_III.png")
+  coord_sf(xlim = c(-8, -1), ylim = c(54.6, 59.55)) +
+  guides(color=guide_legend(title="Nest boxes"), ) +
+  scale_color_manual(values = c("#445690", "#248fc9", "#d5d572"))
+ggsave("figures/sitesMap_VI.png")
 
 

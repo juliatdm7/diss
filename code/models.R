5d325d17d40ecf86391f52192946a42083365d68
juliatdm7 <juliatdm7@gmail.com>
2025-08-12 14:10:28 +0200

    Adding final models for clutch size as well as models for number of fledgelings.

--- code/models.R
+++ code/models.R
@@ -7,12 +7,15 @@ library(glmmTMB)
 library(ggplot2)
 library(DHARMa)
 library(dplyr)
-library(performance)
 
 fed_df <- read.csv("data/fed_df_2025.csv")
 cs_df <- read.csv("data/cs_df_2025.csv")
 suc_df <- read.csv("data/suc_df_2025.csv")
 habitat_foliage_scores <- read.csv("data/Habitat_SiteII.csv")
+
+####################
+### Final Models ###
+####################
 
 ## First egg lay date ##
 
@@ -20,11 +23,12 @@ fed_df$total_FS <- habitat_foliage_scores[match(fed_df$site, habitat_foliage_sco
 
 fed_df$site_year <- as.factor(paste(fed_df$site, fed_df$year, sep="_"))
 
-fed_model_1 <- glmmTMB(fed ~ scale(yo)*scale(total_FS) + scale(w)*scale(total_FS) +  # Fixed effects and their interactions
+fed_model <- glmmTMB(fed ~ scale(yo)*scale(total_FS) + scale(w)*scale(total_FS) +  # Fixed effects and their interactions
                          (1|ring) + (1|site_year),  # random effects
-                       data = fed_df)  # dataset
+                       data = fed_df,
+                       REML = T)  # dataset
 
-summary(fed_model_1)
+summary(fed_model)
 
 
 ## Clutch size ##
@@ -33,14 +37,15 @@ cs_df$total_FS <- habitat_foliage_scores[match(cs_df$site, habitat_foliage_score
 
 cs_df$site_year <- as.factor(paste(cs_df$site, cs_df$year, sep="_"))
 
-cs_model_1 <- glmmTMB(cs ~ scale(yo)*scale(total_FS) + scale(w)*scale(total_FS) +  # Fixed effects and their interactions
+cs_model <- glmmTMB(cs ~ scale(yo)*scale(total_FS) + scale(w)*scale(total_FS) +  # Fixed effects and their interactions
                          (1|ring) + (1|site_year),  # random effects
-                       data = cs_df)  # dataset
+                       data = cs_df,
+                      REML = T)  # dataset
 
-summary(cs_model_1)
+summary(cs_model)
 
 
-## Fledgeling success ##
+## Number of fledgelings ##
 
 suc_df$total_FS <- habitat_foliage_scores[match(suc_df$site, habitat_foliage_scores$Site),"Total"]
 
@@ -48,9 +53,40 @@ suc_df$site_year <- as.factor(paste(suc_df$site, suc_df$year, sep="_"))
 
 suc_model_1 <- glmmTMB(suc ~ scale(yo)*scale(total_FS) + scale(w)*scale(total_FS) +  # Fixed effects and their interactions
                         (1|ring) + (1|site_year),  # random effects
-                       ziformula = ~1,  # specifying a zero-inflated model where we don't expect 0s to arise under a specific predictor a priori
                       data = suc_df,  # dataset
                       family = poisson())  # conditional sampling distribution of the response
 
-summary(suc_model_1)
+summary(suc_model_1)  # AIC = 7154
 
+suc_model_2 <- glmmTMB(suc ~ scale(yo)*scale(total_FS) + scale(w)*scale(total_FS) +  # Fixed effects and their interactions
+                         (1|ring) + (1|site_year),  # random effects
+                       data = suc_df,  # dataset
+                       family = nbinom2())  # conditional sampling distribution of the response
+
+summary(suc_model_2)  # AIC = 7156
+
+suc_model_3 <- glmmTMB(suc ~ scale(yo)*scale(total_FS) + scale(w)*scale(total_FS) +  # Fixed effects and their interactions
+                         (1|ring) + (1|site_year),  # random effects
+                       ziformula = ~1,  # specifying a zero-inflated model where we don't expect 0s to arise under a specific predictor a priori
+                       data = suc_df,  # dataset
+                       family = poisson())  # conditional sampling distribution of the response
+
+summary(suc_model_3)  # AIC = 6797.8
+
+suc_model_4 <- glmmTMB(suc ~ scale(yo)*scale(total_FS) + scale(w)*scale(total_FS) +  # Fixed effects and their interactions
+                         (1|ring) + (1|site_year),  # random effects
+                       ziformula = ~1,  # specifying a zero-inflated model where we don't expect 0s to arise under a specific predictor a priori
+                       data = suc_df,  # dataset
+                       family = nbinom2())  # conditional sampling distribution of the response
+
+summary(suc_model_4)  # AIC = 6799.8
+
+# Number of fledgelings model with the lowest AIC is ZIP
+
+
+#########################
+### Model diagnostics ###
+#########################
+
+
+# First egg lay date
